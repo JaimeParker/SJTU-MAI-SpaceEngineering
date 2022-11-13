212ff858bb1a007ef7680a55c44442245681616b
hazyparker <2559272883@qq.com>
2022-11-13 16:46:02 +0800

    add NA chapter4 exercise4.1-4.3

---
+++ 1_Courses/NumericalAnalysis/homework/Chapter4.md
@@ -0,0 +1,47 @@
+# Chapter 4
+
+Name: Liu Zhaohong
+
+Student ID: 122431910061
+
+## Exercise 4.1
+
+Show that there exists a unique solution to the initial-value problem
+$$
+y'=1+t\sin (ty), \quad 0\leq t\leq2, \quad y(0)=0
+$$
+
+## Exercise 4.2
+
+Solve the following initial-value problem by using Euler's method,
+$$
+y'(t)=y(t)-t^2+1,t\in[0,2],y(0)=0.5
+$$
+Let $h=0.2$ and find a bound for the approximation error.
+
+## Exercise 4.3
+
+Apply Taylor's method of orders (1) two and (2) four with $n=10$ to the initial value problem
+$$
+y'=y-t^2+1, t\in[0,2],y(0)=0.5
+$$
+ 
+
+## Exercise 4.4
+
+Use the Midpoint method and the Modified Euler method with $n=10, h=0.2, t_i=0.2i$ and $y_0=0.5$ to approximate the solution to 
+$$
+y'=y-t^2+1, t\in[0,2],y(0)=0.5
+$$
+
+
+## Exercise 4.5
+
+Use the Runge-Kutta method of order 4 with $h=0.2, 0.1, 0.005$ to approximate the solution to
+$$
+y'=ty+t^3, t\in[0,2],y(0)=1
+$$
+and the exact solution is
+$$
+y=[-2e^{-t^2/2}(\frac{t^2}{2}+1)+3]e^{t^2/2}
+$$
---
+++ 1_Courses/NumericalAnalysis/homework/OED_Solver.py
@@ -0,0 +1,76 @@
+# compose solution for ODEs problems by numerical method
+# reference of ODEs from wiki
+# https://en.wikipedia.org/wiki/Ordinary_differential_equation#General_definition
+# and reference of OEDs' numerical solution
+# https://en.wikipedia.org/wiki/Numerical_methods_for_ordinary_differential_equations
+
+# import libs
+
+
+# class solver for ODEs
+class ODESolver:
+    def __init__(self, df):
+        """init function
+        :param df need to be a function with 2 params, like f(t, y)"""
+        self.df = df  # df is the derivative function (rank 1, 一阶导数)
+
+    def forward_euler(self, init_y, start, end, len_step):
+        """compose forward euler method for a well-posed initial value problem
+        :param init_y the function value at start point, namely y(start)
+        :param start the value of left interval
+        :param end the value of right interval
+        :param len_step the length of step"""
+
+        if end <= start:
+            raise Exception('Error! Bad interval input')
+
+        num_steps = int((end - start) / len_step)
+        # init t and w in f(t, w), avoid using y for misunderstanding
+        t = start
+        w = init_y
+        print("w0 = %f" % w)
+
+        for i in range(num_steps):
+            w += len_step * self.df(t, w)
+            t += len_step
+            index = int(i + 1)
+            print("w%d = %f" % (index, w))
+
+        return w
+
+    def two_order_taylor(self, d2f, init_y, start, end, num_step):
+        """compose forward euler method for a well-posed initial value problem
+        :param d2f f'(t, w)
+        :param init_y the function value at start point, namely y(start)
+        :param start the value of left interval
+        :param end the value of right interval
+        :param num_step the number of step"""
+
+        if end <= start:
+            raise Exception('Error! Bad interval input')
+
+        len_step = (end - start) / num_step
+        # init t and w in f(t, w), avoid using y for misunderstanding
+        t = start
+        w = init_y
+        print("w0 = %f" % w)
+
+        for i in range(num_step):
+            w += len_step * (self.df(t, w) + len_step / 2 * d2f(t, w))
+            t += len_step
+            index = i + 1
+            print("w%d = %f" % (index, w))
+        return w
+
+
+def func4_2(t, y):
+    return y - t * t + 1
+
+
+def func4_3_2(t, y):
+    return y - t * t + 1 - 2 * t
+
+
+test4_2 = ODESolver(func4_2)
+test4_2.forward_euler(0.5, 0, 2, 0.2)
+test4_2.two_order_taylor(func4_3_2, 0.5, 0, 2, 10)
